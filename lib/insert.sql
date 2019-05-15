0806fb4075cce3ac9eddf750ae51f1777274f8e9
Billy Joseph <ingbillyjoseph@gmail.com>
2019-05-15 18:04:48 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
+('Joe Black', 2, 'M', 'Black', 'cool', '1'),
+('Jeanane cowld', 3, 'F', 'Blue pink', 'cool', '1'),
+('Jhon snow', 6, 'M', 'white', 'Blue and gray', 'wrong', '1'),
+('Bob Sponger', 1, 'M', 'white and black', 'wrong', '1'),
+('Katt Joub', 3, 'F', 'black', 'wrong', '0'),
+('Minorista', 2, 'M', 'black', 'wrong', '1'),
+('Bobby spider', 2, 'M', 'black', 'wrong', '1'),
+(NULL, NULL, NULL, NULL, NULL, NULL)

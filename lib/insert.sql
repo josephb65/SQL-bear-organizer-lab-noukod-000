0657bc387e44f67325be7944073ad4a87e9d97cc
Billy Joseph <ingbillyjoseph@gmail.com>
2019-05-15 18:33:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,9 +1,10 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES
-('Mr. Chocolate', 2, 'male', 'white', 'cool', '1'),
-('Rowdy', 3, 'male', 'white', 'cool', '1'),
-('Tabitha', 1, 'female', 'white', 'cool', '1'),
-('Sergeant Brown', 1, 'male', 'white and black', 'wrong', '1'),
-('Melissa', 1, 'female', 'black', 'wrong', '0'),
-('Grinch', 2, 'male', 'black', 'wrong', '1'),
-('Wendy', 2, 'male', 'black', 'wrong', '1'),
-(NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
+© 2019 GitHub, Inc.
